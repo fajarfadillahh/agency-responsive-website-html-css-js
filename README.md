e89788e021c296c161642c9858867522a9306973
fajarfadillahh <fajaragustian979@gmail.com>
2021-12-28 22:34:28 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Responsive Agency Website
+
+## [Download Resource](https://startuplanding.redq.io/)
+
+This website was created using only html, css & javascript following the design from StartupLanding. Click [here](https://startuplanding.redq.io/) to download the design.
+
+![Agency](./img/preview.png)
---
+++ js/main.js
@@ -0,0 +1,90 @@
+// ===== SHOW HEADER MENU =====
+function showMenu(menuId, toggleId, closeId) {
+  const menu = document.getElementById(menuId);
+  const toggle = document.getElementById(toggleId);
+  const close = document.getElementById(closeId);
+
+  if (menu && toggle && close) {
+    toggle.addEventListener("click", () => {
+      menu.classList.add("show-menu");
+    });
+    close.addEventListener("click", () => {
+      menu.classList.remove("show-menu");
+    });
+  }
+}
+showMenu("header-menu", "header-toggle", "header-close");
+
+// ===== STICKY HEADER =====
+const scrollY = window.pageYOffset;
+
+function stickyHeader() {
+  const header = document.getElementById("header");
+
+  this.scrollY >= 5
+    ? header.classList.add("sticky-header")
+    : header.classList.remove("sticky-header");
+}
+window.addEventListener("scroll", stickyHeader);
+
+// ===== ACCORDION FEATURES SECTION =====
+function scrollUp() {
+  const scrollup = document.getElementById("scroll-up");
+
+  this.scrollY >= 150
+    ? scrollup.classList.add("scroll-active")
+    : scrollup.classList.remove("scroll-active");
+}
+window.addEventListener("scroll", scrollUp);
+
+// ===== ACCORDION FEATURES SECTION =====
+const accordionItems = document.querySelectorAll(".features__accordion-item");
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".features__accordion-header");
+
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".features__accordion-content");
+
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
+
+// ===== SCROLL SECTION ACTIVE MENU =====
+const sections = document.querySelectorAll("section[id]");
+
+function sectionActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(`.header__nav a[href*= ${sectionId} ]`)
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(`.header__nav a[href*= ${sectionId} ]`)
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", sectionActive);
